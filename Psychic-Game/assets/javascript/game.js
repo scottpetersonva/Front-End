3f9affc229303056c72ca4888e48165c3bf28cd3
Scott Peterson <scott.peterson.va@gmail.com>
2018-02-21 19:55:19 -0500

    consolidating assignments into one 'homework' folder

--- Psychic-Game/assets/javascript/game.js
+++ Psychic-Game/assets/javascript/game.js
@@ -20,75 +20,90 @@
   // -How do I make sure that the computerGuess function is not resetting after each guess and instead allows the user 9 guesses before picking a new letter?
   // 
 
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Creating variables to hold the number of wins, losses, guesses left, letters guessed.
+  var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var lettersGuessed = [];
-
-
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-
-// This function is run whenever the user presses a key.
+  // Creating variables to hold the number of wins, losses, guesses left, letters guessed.
+  var wins = 0;
+  var losses = 0;
+  var guessesLeft = 9;
+  var lettersGuessed = [];
+  
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  
+  
+  
+  // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
-
-  // Determines which key was pressed.
   
-  var userGuess = event.key;
-
-  // Only run the following code block if the user presses any letter.
-
-    // if (event.key) {
-    //   lettersGuessed.push;
-    // }
+    // Determines which key was pressed.
+    var userGuess = event.key;
   
-    
 
+    if (computerGuess === userGuess) {
+          wins++;
+          guessesLeft=9;
+          lettersGuessed.length=0;
+          computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+          // return to new computer choice
 
-    // if (lettersGuessed.length < 8) {
-    //   guessesLeft === 9;
-    // }
-
-
-    
-
-    if (computerGuess != userGuess) 
+    }
+      
+    else{
         guessesLeft--;
         lettersGuessed.push(userGuess);
-        if (guessesLeft === 0){
-          losses++; 
+      if (lettersGuessed.length === 9) {
+          losses++;
+          guessesLeft=9;
+          lettersGuessed.length=0;
+          computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+          // return to new computer choice
         }
-      
+        }
 
-    if (userGuess === computerGuess) {
-        object.wins++;
+
+        // for console log
+          if (userGuess) {
+        reWriteStats();
+      }
+
+      var html =
+      
+          "<p>wins: " + wins + "</p>" +
+          "<p>losses: " + losses + "</p>" +
+          "<p>guesses left: " + guessesLeft + "</p>" +
+          "<p>letters guessed: " + lettersGuessed + "</p>";
+
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+
+  
+    // Only run the following code block if the user presses any letter.
+  
+      // if (event.key) {
+      //   lettersGuessed.push;
+      // }
+  
+
+      
+  
+    
+      // I want to create a while loop that runs so long as the computer guess does not equal the user guess
+
+      // IF computer guess does not equal user guess DO guesses left -- lettersGuessed.push(userGuess); and nest {if (guessesLeft === 0){losses++;} 
+          
         }
         
       
-      if (userGuess) {
-        reWriteStats();
-      }
-    }
-        // how would i push letters guessed to letterGuessed array?
-    
-    // if (guessesLeft < 1) {
-    //   losses++;
-    //   }
-
-    
-
-
-
-function reWriteStats() {
-  console.log("Wins: " + wins);
-  console.log("Losses: " + losses);
-  console.log("Guesses left: " + guessesLeft);
-  console.log("Letters guessed: " + lettersGuessed);
-  console.log("------------------------------");
-}
+      
+  
+  // for console log
+  
+  function reWriteStats() {
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    console.log("Guesses left: " + guessesLeft);
+    console.log("Letters guessed: " + lettersGuessed);
+    console.log("------------------------------");
+  }
