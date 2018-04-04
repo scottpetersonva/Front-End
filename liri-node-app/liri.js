eb84c0119e4b473bcebbf7f91792e25d8bdac34e
Scott Peterson <scott.peterson.va@gmail.com>
2018-04-04 12:01:38 -0400

    adding liri project to hw folder

---
+++ liri-node-app/liri.js
@@ -0,0 +1,112 @@
+// Linking files with API Keys
+
+require("dotenv").config();
+var keys = require("./keys.js");
+
+// Require code to call APIs
+
+var Twitter = require("twitter")
+var Spotify = require('node-spotify-api');
+var request = require("request");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var terminalEntry = process.argv[2];
+
+// TWITTER TERMINAL ENTRY KEY PHRASE
+
+if (terminalEntry === "my-tweets") {
+
+    var params = {screen_name: '@d3v_d3v_d3v'};
+
+// TWITTER API CALL
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i++) { 
+                console.log(JSON.stringify(tweets[i].text, null, 2));
+            }
+        }
+    });
+
+}
+
+// SPOTIFY SETTING TERMINAL ENTRY KEY PHRASE
+
+if (terminalEntry === "spotify") {
+    
+    var songEntry = process.argv;
+    var songName = "";
+
+    for (var i = 3; i < songEntry.length; i++) {
+        if (i > 3 && i < songEntry.length) {
+            songName = songName + "+" + songEntry[i];
+        }
+
+    else {
+        songName += songEntry[i];
+    }
+}
+
+// SPOTIFY API CALL
+
+  spotify.search({ type: 'track', query: songName, limit: 3 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+    // console.log(JSON.stringify(data, null, 2)); 
+    console.log(JSON.stringify("Track Name: " + data.tracks.items[0].name, null, 2));
+    console.log(JSON.stringify("Artist: " + data.tracks.items[0].album.artists[0].name, null, 2)); 
+    console.log(JSON.stringify("Album Name: " + data.tracks.items[0].album.name, null, 2));
+    console.log(JSON.stringify("Preview URL: " + data.tracks.items[0].album.artists[0].preview_url, null, 2));
+  });
+}
+
+// OMDB SETTING TERMINAL ENTRY KEY PHRASE
+
+if (terminalEntry === "movie-this") {
+    
+    var nodeArgs = process.argv;
+    var movieName = "";
+    var mrNobody = "Mr. Nobody"
+
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+        movieName = movieName + "+" + nodeArgs[i];
+        }
+
+        else {
+        movieName += nodeArgs[i];
+        }
+    }
+
+    var queryUrl
+
+    if (nodeArgs.length > 3) {
+        queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=7138ed1d";
+    }
+
+    else {
+        queryUrl = "http://www.omdbapi.com/?t=" + mrNobody + "&y=&plot=short&apikey=7138ed1d"
+    }
+
+// OMDB API CALL
+
+    request(queryUrl, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+    
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+    
+        }
+    });
+}
