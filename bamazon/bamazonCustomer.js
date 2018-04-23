70161381e33e27e6bfc6a6c824aab244a59d0fe8
Scott Peterson <scott.peterson.va@gmail.com>
2018-04-23 15:36:14 -0400

    finished MySQL Homework

--- bamazon/bamazonCustomer.js
+++ bamazon/bamazonCustomer.js
@@ -2,90 +2,89 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "Longwood1!",
-    database: "bamazon"
-  });
+  host: "localhost",
+  port: 3306,
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Longwood1!",
+  database: "bamazon"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
 
   promptUser()
-  });
   
-  function promptUser() {
-      
-      connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+
+});
+
+function promptUser() {
+
+// Displays stock of items for uster by calling the "bamazon" SQL database and targeting "products" table.  loops through the results and console logs the the length of each column
+
+  connection.query("SELECT * FROM products", function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+    console.log("-----------------------------------");
+
+// Asks user to select the ID of the product they want to buy and the amount of that product that they want to buy
+
+    inquirer.prompt([
+
+      {
+        name: "selectedProduct",
+        type: "input",
+
+        message: "enter the ID of the item would you like to buy"
+      },
+      {
+        name: "productQuantity",
+        type: "input",
+        message: "How many would you like to buy?"
+      }
+
+    ]).then(function (answer) {
+
+// Creates variable to be used in the below connection to the database
+
+      var query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?";
+
+// Sets chosenItem variable so that it selects the row of the chosen ID
+
+      var chosenItem;
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].id == answer.selectedProduct) {
+          chosenItem = res[i];
         }
-        console.log("-----------------------------------");
+      }
 
-        inquirer.prompt([
+// checks to see if the user request is larger than the quantity on hand.  If so, user is notified that the transaction will not go through
 
-          {
-          name: "selectedProduct",
-          type: "input",
-          message: "enter the ID of the item would you like to buy"
-          },
-            
-          {
-          name: "productQuantity",
-          type: "input",
-          message: "How many would you like to buy?"
-          }
-  
-      ]).then(function(answer) {
-        var query = "UPDATE bamazon SET ? WHERE ?";
-        connection.query(query, { selectedProduct: answer.selectedProduct }, function(err, res) {
-          
-          [
-            {
-              stock_quantity: stock_quantity - answer.productQuantity
-            },
-            {
-              id: selectedProduct.id
-            }
-          ]
-          console.log("your total cost is " + answer.productQuantity * 5)
-        });
-        
-      });
+      if (chosenItem.stock_quantity < answer.productQuantity) {
+        console.log("sorry we only have " + chosenItem.stock_quantity + " units of " + chosenItem.product_name + " in stock")
+        connection.end();
+      }
 
-      });
+// if the requested quantity is less than the quantity on hand the stock qunatity is set to the the difference between the stock on and and the user request at the row of the targeted ID
 
-      
+      else {
+
+        connection.query(query, [answer.productQuantity, answer.selectedProduct], function (error) {
+          if (error) throw err;
+          console.log("Order placed successfully!");
+          console.log("Your purchase of " + answer.productQuantity + " " + chosenItem.product_name + " will cost you: " + "$" + answer.productQuantity * chosenItem.price);
+          connection.end();
+        }
+        );
+      }
     }
 
+      );
 
-
-    // function updateProduct() {
-    //   console.log("Updating all Rocky Road quantities...\n");
-    //   var query = connection.query(
-    //     "UPDATE products SET ? WHERE ?",
-    //     [
-    //       {
-    //         quantity: 100
-    //       },
-    //       {
-    //         flavor: "Rocky Road"
-    //       }
-    //     ],
-    //     function(err, res) {
-    //       console.log(res.affectedRows + " products updated!\n");
-    //       // Call deleteProduct AFTER the UPDATE completes
-    //       deleteProduct();
-    //     }
-    //   );
-    
-    //   // logs the actual query being run
-    //   console.log(query.sql);
-    // }
-    
+  });
+}
